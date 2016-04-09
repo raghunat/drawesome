1e532684bd226249e410d7ac73eecac5da51887d
Alexander O'Hare <Alexander O'Hare>
2016-04-09 18:37:28 -0400

    0.1: Added base structure to build on for project and listed reference canvas created by previous canvas group.

---
+++ rawAngular/diff/strokes.js
@@ -0,0 +1,10 @@
+// Contains JS class with methods to store, diff, and recognize
+//   user interaction events with a canvas
+
+class Strokes {
+  // Recognize interactions
+
+  // Be able to interpret interactions and export/store them
+
+  // For test purposes, make them repeat on separate canvas
+}
