ea7d0e84d983a483b1b579379eaf2c4cc5f55b19
Sam McCagg <samuel.mccagg@gmail.com>
2016-04-12 13:30:47 -0400

    first ionic project startup

---
+++ app/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('loginPageCtrl', function($scope) {
+
+})
+   
+.controller('registerCtrl', function($scope) {
+
+})
+   
+.controller('drawesomeCtrl', function($scope) {
+
+})
+   
+.controller('myProfileCtrl', function($scope) {
+
+})
+   
+.controller('moreInformationCtrl', function($scope) {
+
+})
+ 
