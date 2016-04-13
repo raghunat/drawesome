237b17f129b4c874f5ba1731898a70221b05ede6
unknown <ohar4307@DataCommS.ad.fredonia.edu>
2016-04-13 14:00:23 -0400

    0.5: Created getStroke function and added code that would allow to get mouse inputs from canvas

--- rawAngular/diff/strokes.js
+++ rawAngular/diff/strokes.js
@@ -1,9 +1,61 @@
-import {canvas}
+// TODO
+//  May need to include canvas file for color and tool (if just doing strokes,
+//    will need to change code to just get strokes themselves)
+
 // Contains JS class with methods to store, diff, and recognize
 //   user interaction events with a canvas
 class Strokes {
+  // Array of all JSON stroke data
   allStrokes = [];
 
+  // Function to interpret stroke data for other functions to user
+  static getStroke() {
+    // Stroke in JSON format
+    var stroke = {
+      color: "",
+      tool: "",
+      movementData: []
+    };
+    //******If just using "screen" for input, need to change querySelector and
+    //******  variables using mouse methods
+    // Get canvas for input
+    var canvas = document.querySelector('#canvas');
+    // Mouse position variables
+    var mouseX, mouseY,
+    dragging = false;
+
+    //When the mouse is clicked
+    canvas.mousedown(function(e){
+      stroke.color = curColor;
+      stroke.tool = curTool;
+      mouseX = e.pageX - this.offsetLeft;
+      mouseY = e.pageY - this.offsetTop;
+
+      stroke.movementData.push(mouseX,mouseY);
+      dragging = true;
+    });
+
+    //Records drawing when mouse is held
+    canvas.mousemove(function(e){
+      if(dragging){
+        mouseX = e.pageX - this.offsetLeft;
+        mouseY = e.pageY - this.offsetTop;
+        stroke.movementData.push(mouseX,mouseY);
+      }
+    });
+
+    //When mouse is unclicked
+    canvas.mouseup(function(e){
+      dragging = false;
+    });
+
+    //When mouse leaves the canvas
+    canvas.mouseleave(function(e){
+      dragging = false;
+    });
+  }
+
+  // Check to see if stroke is already in array, if not add it to array
   static storeStroke(stroke) {
     let newStroke = Strokes.allStrokes.find(s => {
       if (s.color !== stroke.color || s.tool !== stroke.tool) {
@@ -29,7 +81,7 @@ class Strokes {
     }
   }
 
-  // For test purposes, make them repeat on separate canvas
+  // Draw all strokes on canvas
   static redraw() {
     let canvas = document.querySelector('#canvas');
     let context = canvas.getContext('2d');
