52fe65a63c8abc49e5a29dae288ddc490fa004b0
Shawn Begier <Shawn Begier>
2016-04-19 12:21:48 -0400

    login validation

--- servers/user/server.js
+++ servers/user/server.js
@@ -1,17 +1,32 @@
+//******************************************************************************
+//**************************  INCLUDE MODULES **********************************
+//******************************************************************************
 var express = require('express');
 var app = express();
 path = require('path');
-var Datastore = require('nedb'),
+var Datastore = require('nedb');
+var crypto = require('crypto');
+
+//******************************************************************************
+//**************************  INITIALIZE DATABASE ******************************
+//******************************************************************************
   // Create/Initialize Database
   // (Created as persistent storage, this will create database
   // as a file named 'users' (no file extension)
   userdb = new Datastore({
     filename: 'users.nedb'
   });
+  userdb.loadDatabase();
 
-var crypto = require('crypto');
+//**** Functions ****
+
+//******************************************************************************
+//************************** HASH FUNCTIONS ************************************
+//******************************************************************************
 // use one-way hashing algorithm
 // Will need to encrypt password with a hashing algorithm
+
+// Specific hash for initial user creation
 function hashPassword(user) {
   var pass = user["password"];
   var key = '8Jbn0pe26Aa';
@@ -19,17 +34,71 @@ function hashPassword(user) {
   return user;
 };
 
-userdb.loadDatabase();
+// Hash only the password. Pass plain text password in and return
+// hashed password value
+function hashOnlyPassword(input) {
+  var pass = input;
+  var key = '8Jbn0pe26Aa';
+  hash = crypto.createHmac('sha1', key).update(pass).digest('hex');
+  return hash;
+};
 
-// middleware
+//******************************************************************************
+//**************************  MIDDLEWARE ***************************************
+//******************************************************************************
+
+app.use(require('body-parser').urlencoded({extended:true}));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+//***  Routes ***
 
-// **** Routes ****
+//******************************************************************************
+//**************************  POST REQUESTS ************************************
+//******************************************************************************
+
+// POST request for Login form
+app.post('/login', function (req, res){
+  console.log("Received POST request.");
+  //Check to see if there is actual input for both fields
+  if (req.body.username && req.body.password) {
+    //Hash the input password
+    var hash = hashOnlyPassword(req.body.password);
+
+      userdb.findOne({ username: req.body.username }, function (err, doc) {
+        //First check to see if user is in database
+        if (doc == null) {
+          //If not, inform the user and proceed no further
+          console.log("User not found.");
+          res.status(404).send("User not found.");
+        }
+        else if (doc.password === hash) {
+          //Return object to console
+          console.log("Successful Login.");
+          console.log(doc);
+          //TODO Perform some other Login actions
+              //Redirect to some profile or homepage
+              //Start a Session?
+
+          //Inform user of successful login
+            res.json(doc);
+        }
+      else {
+        //if password doesn't match, do not login
+        console.log("Password does not match. Could not log in.");
+        res.status(403).send("Password does not match. Could not log in.")
+      }
+      });
+  }
+  //If fields are not filled out, inform user and proceed no further
+  else {
+    console.log("One or both fields are empty.");
+    res.status(400).send("One or both fields are empty.");
+  }
+});
 
 // POST to create new users
 app.post('/users', function (req, res) {
@@ -42,15 +111,20 @@ app.post('/users', function (req, res) {
     console.log(query);
     // insert
     userdb.insert(query);
-
   });
-
-  // Will need to encrypt password with a hashing algorithm
-
   // Send response back
   res.send('Added user');
-
 })
+
+//******************************************************************************
+//**************************  GET REQUESTS *************************************
+//******************************************************************************
+
+// GET request for Login page
+app.get('/', function (req, res) {
+  console.log("Received GET request from index.html");
+  res.sendFile(__dirname + '/index.html');
+});
 
 // GET request for listing users?
 app.get('/users', function (req, res) {
@@ -69,9 +143,7 @@ app.get('/users', function (req, res) {
     // users is an array containing all user objects
     res.send(users);
   });
-
 })
-
 
 // GET request for a single user by id #
 app.get('/users/:id', function (req, res) {
@@ -84,6 +156,9 @@ app.get('/users/:id', function (req, res) {
   });
 })
 
+//******************************************************************************
+//**************************  PUT REQUESTS *************************************
+//******************************************************************************
 
 // PUT request for a user update
 app.put('/users/:id', function (req, res) {
@@ -113,6 +188,9 @@ app.put('/users/:id', function (req, res) {
   res.send('Updated user');
 })
 
+//******************************************************************************
+//**************************  DELETE REQUESTS **********************************
+//******************************************************************************
 
 // DELETE request for a user by id
 app.delete('/users/:id', function (req, res) {
@@ -128,6 +206,9 @@ app.delete('/users/:id', function (req, res) {
     });
 })
 
+//******************************************************************************
+//**************************  SERVER INFORMATION *******************************
+//******************************************************************************
 
 // Set up server to listen on a port
 var server = app.listen(8081, function () {
