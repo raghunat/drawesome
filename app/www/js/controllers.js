863fb88676cf804d4f620c49efd7587ce3e4aba9
Wayne Warnke <Wayne Warnke>
2016-04-29 21:34:48 -0400

    profile controller using userapi to display info

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -12,8 +12,22 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('myProfileCtrl', function($scope) {
+.controller('myProfileCtrl', function($scope, $http) {
+  //When profile opens... run this...
 
+  $scope.loadingUser = true;
+  $http.get('http://localhost:8081/users/c9suHUY5myxN8OE7')
+  .success(function(users){
+    $scope.username = users.username;
+    $scope.firstName = users.firstname;
+    $scope.lastName = users.lastname;
+  })
+  .error(function(error){
+    $scope.error = error;
+  })
+  .finally(function(){
+    $scope.loadingUser = false;
+  });
 })
 
 .controller('moreInformationCtrl', function($scope) {
